cf42c8ec9ae566c9c4675330c3f0cb522d6e8951
Rakesh Narang <rakeshnarang5@gmail.com>
2020-04-29 14:25:45 +0530

    Update OnlyNonDuplicate.java

--- OnlyNonDuplicate.java
+++ OnlyNonDuplicate.java
@@ -1,29 +1,29 @@
 // time complexity -> O(n)
 // space complexity -> O(n) in the worst case
 class Solution {
- public int singleNumber(int[] nums) {
-  // we use a set to store value we've seen
-  Set < Integer > set = new HashSet < > ();
-  // traverse array once
-  for (int i = 0; i < nums.length; i++) {
-   // if we have already seen the number
-   // remove from set
-   if (set.contains(nums[i])) {
-    set.remove(nums[i]);
-   } else {
-    // else add number to set
-    set.add(nums[i]);
-   }
-  }
-  // at the end, only that number will be in set
-  // which occurs only once
-  // and that's your answer
-  if (set.size() >= 1) {
-   for (Integer i: set) {
-    return i;
-   }
-  }
-  // in all other cases, return -1
-  return -1;
- }
+	public int singleNumber(int[] nums) {
+		// we use a set to store value we've seen
+		Set < Integer > set = new HashSet < >();
+		// traverse array once
+		for (int i = 0; i < nums.length; i++) {
+			// if we have already seen the number
+			// remove from set
+			if (set.contains(nums[i])) {
+				set.remove(nums[i]);
+			} else {
+				// else add number to set
+				set.add(nums[i]);
+			}
+		}
+		// at the end, only that number will be in set
+		// which occurs only once
+		// and that's your answer
+		if (set.size() >= 1) {
+			for (Integer i: set) {
+				return i;
+			}
+		}
+		// in all other cases, return -1
+		return - 1;
+	}
 }
