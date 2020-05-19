d6d32290d6981d3f4d47875f54b99a648330bb44
Rakesh Narang <rakeshnarang5@gmail.com>
2020-05-20 01:53:15 +0530

    Update FindFirstUniqueChar.java

--- FindFirstUniqueChar.java
+++ FindFirstUniqueChar.java
@@ -1,10 +1,12 @@
 /*
-* we will use a count tracker class to store characters, it's occurrences and the last idx
-*/
+ * we will use a count tracker class to store characters, it's occurrences and
+ * the last idx
+ */
 class CountTracker {
 	char character;
 	int numOccurrence;
 	int idx;
+
 	public CountTracker(char character, int numOccurrence, int idx) {
 		super();
 		this.character = character;
@@ -15,11 +17,14 @@ class CountTracker {
 
 class Solution {
 	/*
-    * we will take a linked hashmap -> store custom object for character in the hashmap -> if char is new, count will be new -> else count will be incremented -> at last, we will will check all the characters in the map and for the character where the count is 1, we will return the idx of that character -> else we will return -1
-    */
+	 * we will take a linked hashmap -> store custom object for character in the
+	 * hashmap -> if char is new, count will be new -> else count will be
+	 * incremented -> at last, we will will check all the characters in the map and
+	 * for the character where the count is 1, we will return the idx of that
+	 * character -> else we will return -1
+	 */
 	public int firstUniqChar(String s) {
-		Map < Character,
-		CountTracker > seenMap = new LinkedHashMap < >();
+		Map<Character, CountTracker> seenMap = new LinkedHashMap<>();
 		for (int i = 0; i < s.length(); i++) {
 			if (seenMap.containsKey(s.charAt(i))) {
 				seenMap.get(s.charAt(i)).numOccurrence++;
@@ -27,9 +32,10 @@ class Solution {
 				seenMap.put(s.charAt(i), new CountTracker(s.charAt(i), 1, i));
 			}
 		}
-		for (Character ch: seenMap.keySet()) {
-			if (seenMap.get(ch).numOccurrence == 1) return seenMap.get(ch).idx;
+		for (Character ch : seenMap.keySet()) {
+			if (seenMap.get(ch).numOccurrence == 1)
+				return seenMap.get(ch).idx;
 		}
-		return - 1;
+		return -1;
 	}
 }
