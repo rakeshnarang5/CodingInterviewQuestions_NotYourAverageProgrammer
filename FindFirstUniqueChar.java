f39b464351a77eb9c2cce55eb7097f488d8cc2cd
Rakesh Narang <rakeshnarang5@gmail.com>
2020-05-20 01:52:07 +0530

    Update FindFirstUniqueChar.java

--- FindFirstUniqueChar.java
+++ FindFirstUniqueChar.java
@@ -1,3 +1,6 @@
+/*
+* we will use a count tracker class to store characters, it's occurrences and the last idx
+*/
 class CountTracker {
 	char character;
 	int numOccurrence;
@@ -11,6 +14,9 @@ class CountTracker {
 }
 
 class Solution {
+	/*
+    * we will take a linked hashmap -> store custom object for character in the hashmap -> if char is new, count will be new -> else count will be incremented -> at last, we will will check all the characters in the map and for the character where the count is 1, we will return the idx of that character -> else we will return -1
+    */
 	public int firstUniqChar(String s) {
 		Map < Character,
 		CountTracker > seenMap = new LinkedHashMap < >();
