8958988a959c6b400ff1e45714f33bcd6e7f7af0
Rakesh Narang <rakeshnarang5@gmail.com>
2021-11-26 00:57:27 +0530

    Update ConvertHashtagsToCSV.java

--- ConvertHashtagsToCSV.java
+++ ConvertHashtagsToCSV.java
@@ -1,1 +1,16 @@
 
+public class Main
+{
+	public static void main(String[] args) {
+		String input = "#softwareengineering #softwaredeveloper #programming #softwareengineer #software #coding #softwaredevelopment #programmer #developer #computerscience #javascript #coder #engineering #webdevelopment #python #java #webdeveloper #codinglife #html #programmers #programminglife #technology #css #developers #code #programmerlife #coders #softwaretesting #coderlife #bhfyp";
+		String[] split = input.split("#");
+		StringBuilder output = new StringBuilder();
+		for(int i = 1 ; i < split.length ; i++) {
+		    output.append(split[i]);
+		    if(i != split.length-1){
+		        output.append(",");
+		    }
+		}
+		System.out.println(output);
+	}
+}
